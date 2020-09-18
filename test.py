2ddb94c232ac8ba4959b05c9b67c7d0b55af072e
zoo868e <zoo868e@gmail.com>
2020-09-19 03:04:05 +0800

    Complete to draw all bisection
    Calculate all bisection and draw it.
    Add buttom run to start to calculate the edge and plumb edge.
    Then draw the plumb edge.
    
    Add the function clean, draw_node, draw_plumb_edge, gen_edge, run.
    draw_node to draw the nodes in list_node.
    draw_plumb_edge to draw the plumb edge which pass in.
    clean to remove both list_node and list_edge also clear the frame.
    gen_edge to generate all edge between every two node.
    run to start to calculate the edge and draw it's plumb_edge.
    
    Fix the bug when x1==x2 and y1==y2 in Edge(N1,N2).
    At that time return Edge(Node(0,0),Node(0,0)).
    To avoid this edge.

--- test.py
+++ test.py
@@ -14,29 +14,33 @@ canvas_width = 600
 canvas_height = 600
 list_node = []
 list_edge = []
+plumb_edge = []
 
 def paint(event):
 	python_green = "#476042"
 	list_node.append(Node(event.x,event.y))
-	x1, y1 = (event.x - 1), (event.y - 1)
-	x2, y2 = (event.x + 1), (event.y + 1)
-	w.create_oval(x1, y1, x2, y2, fill=python_green)
+	#x1, y1 = (event.x - 1), (event.y - 1)
+	#x2, y2 = (event.x + 1), (event.y + 1)
+	#w.create_oval(x1, y1, x2, y2, fill=python_green)
+	draw_node()
 	print("x = "+str(list_node[len(list_node)-1].x)+",y = "+str(list_node[len(list_node)-1].y))
-	if len(list_node) > 1:
-		list_edge.append(Edge(list_node[len(list_node)-2],list_node[len(list_node)-1]))
-		plot = plumb(list_edge[len(list_edge)-1])
-		w.create_line(plot.node1.x,plot.node1.y,plot.node2.x,plot.node2.y)
-		print("Edge plot: (" + str(plot.node1.x) + "," + str(plot.node1.y) + ") to (" + str(plot.node2.x) + "," + str(plot.node2.y) + ")")
-		#w.create_line(list_edge [len ( list_edge) - 1].node1.x,list_edge[len(list_edge)-1].node1.y,list_edge[len(list_edge)-1].node2.x,list_edge[len(list_edge)-1].node2.y)
-		#print("Edge append: (" + str( list_edge [len ( list_edge) - 1].node1.x) + "," + str(list_edge[len(list_edge)-1].node1.y) + "),(" + str(list_edge[len(list_edge)-1].node2.x) + "," + str(list_edge[len(list_edge)-1].node2.y) + ")")
+	#if len(list_node) > 1:
+	#	gen_edge(list_node[len(list_node)-1])
+	#	draw_node()
+	#	for edge in list_edge:
+	#		draw_plumb_edge(edge)
 
 def plumb(edge):
 	x1, y1 = (edge.node1.x,edge.node1.y)
 	x2, y2 = (edge.node2.x,edge.node2.y)
 	x_mid = (x1+x2)/2
 	y_mid = (y1+y2)/2
+	if (x2-x1) == 0 or (y2-y1) == 0:
+		return Edge(Node(0,0),Node(0,0))
 	old_k = (y2-y1)/(x2-x1)
 	m = -(x2-x1)/(y2-y1)
+	if m == 0:
+		return Edge(Node(0,0),Node(0,0))
 	b = y_mid - m*x_mid
 	if (-b)/m < 0:
 		node1 = Node(600,600*m+b)
@@ -49,6 +53,35 @@ def plumb(edge):
 	plumb_edge = Edge(node1,node2)
 	return plumb_edge
 
+
+def clean():
+	w.delete('all')
+	list_node.clear()
+	list_edge.clear()
+
+def draw_node():
+	for i in range(0,len(list_node)):
+		x1, y1 = (list_node[i].x-1), (list_node[i].y-1)
+		x2, y2 = (list_node[i].x+1), (list_node[i].y+1)
+		w.create_oval(x1, y1, x2, y2, fill='black')
+	
+def draw_plumb_edge(edge):
+	plumb_edge = plumb(edge)
+	w.create_line(plumb_edge.node1.x, plumb_edge.node1.y, plumb_edge.node2.x, plumb_edge.node2.y)
+	print("Edge plot: (" + str(plumb_edge.node1.x) + "," + str(plumb_edge.node1.y) + ") to (" + str(plumb_edge.node2.x) + "," + str(plumb_edge.node2.y) + ")")
+
+def gen_edge(node):
+	for i in range(0,len(list_node)-1):
+		list_edge.append(Edge(node,list_node[i]))
+
+def run():
+	list_edge.clear()
+	print("clear the list_edge")
+	for i in range(0,len(list_node)-1):
+		for j in range(i+1,len(list_node)):
+			list_edge.append(Edge(list_node[i],list_node[j]))
+			draw_plumb_edge(list_edge[len(list_edge)-1])
+
 master = Tk()
 master.title("Points")
 master.resizable(0,0)
@@ -59,7 +92,10 @@ w.bind("<ButtonRelease-1>", paint)
 message = Label(master, text="Press and Drag the mouse to draw")
 message.pack(side=BOTTOM)
 
-btn = Button(master, text="btn")
-btn.pack()
+btn_clear = Button(master, text="clear",command=clean)
+btn_clear.pack()
+
+btn_run = Button(master, text="Run",command = run)
+btn_run.pack()
 
 mainloop()
