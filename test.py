9cc4e4c52770f7ed275b19077f3533ecc8eb0341
zoo868e <zoo868e@gmail.com>
2020-09-21 01:03:27 +0800

    Draw 3-point voronoi diagram already
    Can draw the 3-point voronoi diagram which all angle in triangle
    are less then 90-degree.
    If exists a degree larger or equal to 90-degree.
    The line will draw the wrong direction.

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ canvas_width = 600
 canvas_height = 600
 list_node = []
 list_edge = []
-plumb_edge = []
+edge = []
 
 def paint(event):
 	python_green = "#476042"
@@ -28,7 +28,7 @@ def paint(event):
 	#	gen_edge(list_node[len(list_node)-1])
 	#	draw_node()
 	#	for edge in list_edge:
-	#		draw_plumb_edge(edge)
+	#		draw_edge(edge)
 
 def plumb(edge):
 	x1, y1 = (edge.node1.x,edge.node1.y)
@@ -50,8 +50,8 @@ def plumb(edge):
 		node2 = Node((600-b)/m,600)
 	else:
 		node2 = Node(0,b)
-	plumb_edge = Edge(node1,node2)
-	return plumb_edge
+	edge = Edge(node1,node2)
+	return edge
 
 
 def clean():
@@ -65,10 +65,9 @@ def draw_node():
 		x2, y2 = (list_node[i].x+1), (list_node[i].y+1)
 		w.create_oval(x1, y1, x2, y2, fill='black')
 	
-def draw_plumb_edge(edge):
-	plumb_edge = plumb(edge)
-	w.create_line(plumb_edge.node1.x, plumb_edge.node1.y, plumb_edge.node2.x, plumb_edge.node2.y)
-	print("Edge plot: (" + str(plumb_edge.node1.x) + "," + str(plumb_edge.node1.y) + ") to (" + str(plumb_edge.node2.x) + "," + str(plumb_edge.node2.y) + ")")
+def draw_edge(edge):
+	w.create_line(edge.node1.x, edge.node1.y, edge.node2.x, edge.node2.y)
+	print("Edge plot: (" + str(edge.node1.x) + "," + str(edge.node1.y) + ") to (" + str(edge.node2.x) + "," + str(edge.node2.y) + ")")
 
 def gen_edge(node):
 	for i in range(0,len(list_node)-1):
@@ -77,10 +76,39 @@ def gen_edge(node):
 def run():
 	list_edge.clear()
 	print("clear the list_edge")
-	for i in range(0,len(list_node)-1):
-		for j in range(i+1,len(list_node)):
-			list_edge.append(Edge(list_node[i],list_node[j]))
-			draw_plumb_edge(list_edge[len(list_edge)-1])
+	for i in range(0,len(list_node)-2):
+		for j in range(i+1,len(list_node)-1):
+			for k in range(j+1,len(list_node)):
+				draw_3point(list_node[i],list_node[j],list_node[k])
+				node = cal_Circumscribed(list_node[i],list_node[j],list_node[k])
+				w.create_oval(node.x-5,node.y-5,node.x+5,node.y+5,fill="red")
+			#list_edge.append(Edge(list_node[i],list_node[j]))
+			#draw_edge(list_edge[len(list_edge)-1])
+
+def cal_Circumscribed(node1,node2,node3):
+	edge1 = plumb(Edge(node1,node2))
+	edge2 = plumb(Edge(node1,node3))
+	a1 = (edge1.node1.y - edge1.node2.y)/(edge1.node1.x - edge1.node2.x)
+	b1 = edge1.node1.y - (edge1.node1.x * a1)
+	a2 = (edge2.node1.y - edge2.node2.y)/(edge2.node1.x - edge2.node2.x)
+	b2 = edge2.node1.y - (edge2.node1.x * a2)
+	node = Node((b2-b1)/(a1-a2),a1*(b2-b1)/(a1-a2)+b1)
+	return node
+
+def draw_3point(nodea,nodeb,nodec):
+	node_cir = cal_Circumscribed(nodea,nodeb,nodec)
+	node1 = Node((nodea.x+nodeb.x)/2,(nodea.y+nodeb.y)/2)
+	node2 = Node((nodec.x+nodeb.x)/2,(nodec.y+nodeb.y)/2)
+	node3 = Node((nodea.x+nodec.x)/2,(nodea.y+nodec.y)/2)
+
+	edge1 = Edge(node_cir,Node(node1.x+600*(node1.x-node_cir.x),node1.y+600*(node1.y-node_cir.y)))
+	draw_edge(edge1)
+	edge2 = Edge(node_cir,Node(node2.x+600*(node2.x-node_cir.x),node2.y+600*(node2.y-node_cir.y)))
+	draw_edge(edge2)
+	edge3 = Edge(node_cir,Node(node3.x+600*(node3.x-node_cir.x),node3.y+600*(node3.y-node_cir.y)))
+	draw_edge(edge3)
+
+
 
 master = Tk()
 master.title("Points")
